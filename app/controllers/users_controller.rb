7d5e867e6028b66238a886edd992508423d365c6
Jeff Hamm <jeffhamm@me.com>
2014-04-30 22:13:34 -0400

    Making user model and secure passswords

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
-  def new
+  def create
+  	@user = User.new(user_params)
   end
+
+  private 
+  	def user_params
+  		params.require(:name).require(:email).permit(:password, :password_confirmation)
+  	end
 end
