48b813ccfa97722e6adf8255de53aec6c6fc1362
Jeff Hamm <jeffhamm@me.com>
2014-05-08 10:38:00 -0400

    Finish user signup

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -1,3 +1,5 @@
+require "spec_helper"
+
 feature "Static pages" do
   before { visit signup_path }
   scenario "Signup page" do
@@ -8,32 +10,34 @@ end
 feature "profile page" do
 
 	let(:user) { FactoryGirl.create(:user) }
-
-	before {visit user_path(user)}
+	before { visit user_path(user) }
 
 	scenario "profile pages " do
       expect(page).to have_selector('h1', user.name)
       expect(page).to have_title(user.name)
   end
 
-  scenario "signup" do
-    before { visit signup_path }
-    let(:submit) {"Create my account"}
-    scenario "with invalid information" do
-      expect { click_button submit }.not_to change(User, :count)
-    end
-    
-    scenario "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar1"
-        fill_in "Confirmation", with: "foobar1"
-      end
+end
 
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
-      end
-    end
+
+feature "signup" do
+
+  before { visit signup_path }
+  let(:submit) {"Create my account"}
+  
+  scenario "with invalid information" do
+    expect { click_button submit }.not_to change(User, :count)
   end
+
+  scenario "with valid information" do
+  
+    fill_in "Name",         with: "Example User"
+    fill_in "Email",        with: "user@example.com"
+    fill_in "Password",     with: "foobar1"
+    fill_in "Confirmation", with: "foobar1"
+
+    expect { click_button submit }.to change(User, :count).by(1)
+
+   end
+
 end
